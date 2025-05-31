c8673c736c4f981e1844ea59c587e3f191c2bcca
Mohamed Al-Rasbi <mohamed.rasbi@rihal.om>
2025-05-31 12:20:28 +0400

    nvim config: format on save

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,9 +1,16 @@
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "typescript", "typescriptreact" },
-  callback = function()
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
-    vim.bo.softtabstop = 2
-    vim.bo.expandtab = true
-  end,
+    pattern = { "typescript", "typescriptreact" },
+    callback = function()
+        vim.bo.tabstop = 2
+        vim.bo.shiftwidth = 2
+        vim.bo.softtabstop = 2
+        vim.bo.expandtab = true
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = buffer,
+    callback = function()
+        vim.lsp.buf.format { async = false }
+    end
 })
